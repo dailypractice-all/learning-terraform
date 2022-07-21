3daff8b631accfc344d7e9ffc42f53d51fd3dc21
gaurav1996b <1996gauravb@gmail.com>
2022-07-21 15:47:14 +0530

    updating terraform file

--- sample1/main.tf
+++ sample1/main.tf
@@ -25,7 +25,7 @@ resource "aws_security_group" "allow_ssh" {
 }
 
 resource "aws_instance" "web" {
-  ami           = ami-031303182815afa6e
+  ami           = "ami-031303182815afa6e"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   tags = {
