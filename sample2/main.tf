28bbbea47b1aea6c94b201e4b520877f582dac6e
gaurav1996b <1996gauravb@gmail.com>
2022-07-21 16:54:56 +0530

    updating terraform file

--- sample2/main.tf
+++ sample2/main.tf
@@ -5,3 +5,11 @@ module "sg" {
 module "instance" {
   source = "./instance"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-b-1996"
+    key    = "sample2/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
