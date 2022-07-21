4c99d514b617036ed40df6e17db05ba19bc69fca
gaurav1996b <1996gauravb@gmail.com>
2022-07-21 15:44:57 +0530

    updating terraform file

---
+++ sample1/main.tf
@@ -0,0 +1,36 @@
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "vpc-08ae3a8067de96ca7"
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = ami-031303182815afa6e
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  tags = {
+    Name = "terraform"
+  }
+}
+
+// ingress means inboundaries and exgress means out boundaries
