ca5906e27775542251c67f20071b5b6105a17f34
gaurav1996b <1996gauravb@gmail.com>
2022-07-22 12:17:46 +0530

    updating terraform file

--- 04-loops/approch02/module/main.tf
+++ 04-loops/approch02/module/main.tf
@@ -2,6 +2,8 @@ resource "aws_instance" "web" {
   ami           = "ami-031303182815afa6e"
   instance_type = "t2.micro"
   tags = {
-    Name = "terraform"
+    Name = var.nametag
   }
 }
+
+variable "nametag" {}
