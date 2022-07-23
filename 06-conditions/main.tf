6093c8846c7ca1b5b881901dde174e891ced05bd
gaurav1996b <1996gauravb@gmail.com>
2022-07-23 11:06:59 +0530

    updating terraform file

--- 06-conditions/main.tf
+++ 06-conditions/main.tf
@@ -1,8 +1,6 @@
 resource "aws_instance" "web" {
-  // count      = 0 (We can make some condition to come with a value based on that this resource can be created)
   ami           = data.aws_ami.example.image_id
   instance_type = local.instance_type
-
   tags = {
     Name = "terraform"
   }
@@ -22,3 +20,5 @@ data "aws_ami" "example" {
   name_regex       = "base-with-ansible"
   owners           = ["self"]
 }
+
+// count      = 0 (We can make some condition to come with a value based on that this resource can be created)
